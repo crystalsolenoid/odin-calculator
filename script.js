cc8b0d424c2647dc219eab2c499da02869c37f1b
Quinten Konyn <qkonyn@gmail.com>
2023-07-07 05:32:18 -0700

    Enable numbers, CE button, and digital display
    CE stands for 'clear entry' and clears only the current number being entered.
    The C button will clear both the current number and any previous number
    and operator input as well, but it is not yet implemented.
    
    The current number `displayValue` is stored as a string, so that decimal
    points will work correctly later.

--- script.js
+++ script.js
@@ -30,3 +30,42 @@ function operate(operator, a, b) {
       return divide(a, b);
   }
 }
+
+let displayValue;
+let display = document.querySelector('#output');
+clearEntry();
+
+function writeOutput() {
+  if (displayValue === '') {
+    display.innerText = '0';
+  } else {
+    display.innerText = displayValue;
+  }
+}
+
+function clearEntry() {
+  displayValue = '';
+  writeOutput();
+}
+
+function appendDisplayValue(value) {
+  let digit = value.toString();
+  displayValue += digit;
+  writeOutput();
+}
+
+function numberPress(e) {
+  let value = e.target.id[1];
+  if (displayValue.length === 14) return; // don't overflow display
+  appendDisplayValue(value);
+}
+
+let numNodeList = document.querySelectorAll('.number');
+numNodeList.forEach(node => {
+  if (node.id === "point") {
+  } else {
+    node.addEventListener('click', numberPress);
+  }
+});
+
+document.querySelector('#ce').addEventListener('click', clearEntry);
